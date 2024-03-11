9ca91104c5e1e63990624ea192180d790470b2a7
Shuaib Salam <shuaibsalam1234@gmail.com>
2024-03-11 11:23:37 +0530

    final commit

--- index.js
+++ index.js
@@ -11,30 +11,29 @@ io.on('connection', (socket)=>{
     // console.log('connected', socket.id);
 
     socket.on('room:join', (data)=>{
-        const { email, room, name} = data; 
-        // console.log('joined room', room);
+        const {email, room, name, topic, joinName} = data; 
+        console.log('joined room', room, topic, name, joinName);
         emailToSocketIdMap.set(email,socket.id);
         socketidToEmailMap.set(socket.id , email);
-        io.to(room).emit('user:joined',{email, id:socket.id, name})
+        io.to(room).emit('user:joined',{ id:socket.id, name, topic, joinName})
         socket.join(room)
         io.to(socket.id).emit('room:join',data)
     });
 
     socket.on('room:details',(data)=>{    
         const {topic, room, name} = data;
-        console.log(topic, room, name);
         io.to(room).emit('getRoom:details',data)
-    })
+    }) 
 
     socket.on('send:JoinerName', ({joinName, room})=>{
-        console.log(joinName, room);
         io.to(room).emit('get:joinerName',joinName)
     })
 
-    socket.on('user:call', ({ to, offer})=>{
-        io.to(to).emit('incoming:call', {from : socket.id, offer})
+    socket.on('user:call', ({ to, offer,remoteUserName, topic, currentUserName})=>{
+        console.log(remoteUserName);
+        io.to(to).emit('incoming:call', {from : socket.id, offer, remoteUserName, topic, currentUserName})
     });
-  
+   
     socket.on('call:accepted', ({to , ans})=>{
         io.to(to).emit('call:accepted', {from : socket.id, ans})
     })
